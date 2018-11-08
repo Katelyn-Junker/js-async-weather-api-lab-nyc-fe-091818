2808e61d9f2102ce208d7b2055b6b244a0201a78
Katelyn Junker <kjunker@villanova.edu>
2018-11-08 23:49:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function handleFormSubmit(event) {
 
 function fetchCurrentWeather(city) {
   //fetch current weather based on city
-  fetch('http://api.openweathermap.org/data/2.5/weather?' + city + )
+  fetch('http://api.openweathermap.org/data/2.5/weather?' + city + '')
 }
 
 function displayCurrentWeather(json) {
